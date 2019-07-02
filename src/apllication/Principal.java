700f1ffb70cdefeff346d5a310fbacbfc43da80d
Alexandre Pereira <bryanalexandre13@hotmail.com>
2019-07-02 15:00:04 -0300

    Iniciando estudos

---
+++ src/apllication/Principal.java
@@ -0,0 +1,18 @@
+package apllication;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	   Scanner sc  = new Scanner(System.in);
+	   System.out.println("Entre com seu nome");
+	   String nome =  sc.next(); 
+	   System.out.println("Seja bem vindo "+nome);
+	 
+	   
+	 
+
+	}
+
+}
